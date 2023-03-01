93b0b1d9406f2ad347a9881d06c0cdae53ac3072
AlexDiazKGS <alexanderd@kgsresearch.com>
2023-03-01 12:59:04 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+
+function App() {
+  const [data, setData] = useState([{}])
+  const [data2, setData2] = useState([{}])
+  const [transcript, setTranscript] = useState([{}])
+
+  useEffect(() => {
+    fetch("/members").then(
+      res => res.json()
+      ).then(
+        data => {
+          setData(data)
+        }
+      )
+  }, [])
+
+  useEffect(() => {
+    fetch("/members2").then(
+      res2 => res2.json()
+      ).then(
+        data2 => {
+          setData2(data2)
+        }
+      )
+  }, [])
+
+  useEffect(() => {
+    fetch("/transcript").then(
+      result => result
+      ).then(
+        output => {
+          setTranscript(output)
+          console.log(output)
+        }
+      )
+  }, [])
+
+  return (
+    <div>
+      {(typeof data.members === 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+        data.members.map((member, i) => (
+          <p key={i}>{member}</p>
+        ))
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ flask-server/server.py
@@ -0,0 +1,35 @@
+from flask import Flask
+import os
+import whisper
+from pytube import YouTube
+import gradio as gr
+
+app = Flask(__name__)
+
+model = whisper.load_model("base")
+ytURL = "https://www.youtube.com/watch?v=sagpI6DSgeE"
+
+# Members API Route
+@app.route("/members")
+def members():
+    return {"members": ["Member1", "Member2","Member3"]}
+
+@app.route("/members2")
+def members2():
+    return {"members": ["Member3", "Member4","Member5"]}
+
+@app.route("/transcript")
+def getTranscript():
+  yt = YouTube("https://www.youtube.com/watch?v=sagpI6DSgeE")
+  video = yt.streams.filter(only_audio=True).first()
+  out_file=video.download(output_path=".")
+  base, ext = os.path.splitext(out_file)
+  new_file = base+'.mp3'
+  os.rename(out_file, new_file)
+  result = model.transcribe(new_file)
+  output = result['text'].strip()
+  os.remove(new_file)
+  return output
+
+if __name__ == "__main__":
+    app.run(debug=True)
